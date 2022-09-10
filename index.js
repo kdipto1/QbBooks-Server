4046c836f92a6f5bcdb851eddb30e1666406cb3e
kdipto1 <kdipto@outlook.com>
2022-09-10 21:00:14 +0600

    mongodb connected

---
+++ index.js
@@ -0,0 +1,40 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const jwt = require("jsonwebtoken");
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb connect
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p85dy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const booksCollection = client.db("qbBooks").collection("books");
+    //post item in database
+    app.post("/book", async (req, res) => {
+      const newBook = req.body;
+      const result = await booksCollection.insertOne(newBook);
+      res.send(result);
+    })
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Listening from port ${port}`);
+});
